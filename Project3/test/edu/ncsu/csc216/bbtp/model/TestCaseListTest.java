c7c1e3fd0134e5eb0565aaa93f5df61c7c3945b6
Owner <Owner@Owner-PC>
2017-04-16 17:50:32 -0400

    Added TestCaseListTests

--- Project3/test/edu/ncsu/csc216/bbtp/model/TestCaseListTest.java
+++ Project3/test/edu/ncsu/csc216/bbtp/model/TestCaseListTest.java
@@ -33,10 +33,13 @@ public class TestCaseListTest {
 	{
 		TestCaseList test = new TestCaseList("TestCaseList", "TCL");
 		TestingType testType = new TestingType("test type", "test type", "test type");
-		Date testDate = new Date();
+		Date testDate1 = new Date();
+		Date testDate2 = new Date();
+		Date testDate3 = new Date();
+		Date testDate4 = new Date();
+		Date testDate5 = new Date();
 		
-		
-		test.addTestCase("description", testType, testDate, "expected", false, null, null, false);
+		test.addTestCase("description", testType, testDate1, "expected", false, null, null, false);
 		assertEquals(1, test.size());
 		assertFalse(test.isEmpty());
 		
@@ -57,14 +60,18 @@ public class TestCaseListTest {
 		assertTrue(testArray[0][0].equals("TCL-TC1"));
 		assertTrue(testArray[0][1].equals("description"));
 		assertTrue(testArray[0][2].equals(testType));
-		assertTrue(testArray[0][3].equals(testDate));
+		assertTrue(testArray[0][3].equals(testDate1));
 		assertTrue(testArray[0][4] == null);
 		assertTrue(testArray[0][5].equals(false));
 		assertTrue(testArray[0][6].equals("expected"));
 		assertTrue(testArray[0][7] == null);
 		assertTrue(testArray[0][8].equals(false));
 	
-		assertTrue(test.addTestCase("description", testType, testDate, "expected", false, null, null, false));
+		assertTrue(test.addTestCase("description", testType, testDate1, "expected", false, null, "actual", false));
+		assertTrue(test.addTestCase("description", testType, testDate2, "expected", true, testDate4, "actual", false));
+		assertTrue(test.addTestCase("description", testType, testDate3, "expected", false, null, "actual", false));
+		assertTrue(test.addTestCase("description", testType, testDate4, "expected", true, testDate5, "actual", false));
+		assertTrue(test.addTestCase("description", testType, testDate5, "expected", false, null, "actual", false));
 		
 		assertTrue(test.removeTestCase("TCL-TC1"));
 		
